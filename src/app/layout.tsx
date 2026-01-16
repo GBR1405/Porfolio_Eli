973e8e53cdc932fba5c4e1e9a1cae746ffdee067
GBR1405 <124008178+GBR1405@users.noreply.github.com>
2026-01-16 02:33:20 -0600

    Intento #3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "./components/layout/header";
-import Footer from "./components/layout/footer";
+import Header from "./components/layout/header/header";
+import Footer from "./components/layout/footer/footer";
 import ThemeProvider from "./components/theme-provider/theme-provider";
 import PageLoader from "./components/ui/page-loader";
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import Divider from "./components/divider";
-import AboutMe from "./components/home/about-me";
-import Education from "./components/home/education";
-import FeaturedWork from "./components/home/featured-work";
-import HeroSection from "./components/home/hero-section";
-import ProjectOverview from "./components/home/project-overview";
+import AboutMe from "./components/home/about-me/aboutme";
+import Education from "./components/home/education/education";
+import FeaturedWork from "./components/home/featurework/featurework";
+import HeroSection from "./components/home/hero-section/herosection";
+import ProjectOverview from "./components/home/project-overview/projectoveriew";
 
 const page = () => {
   return (
